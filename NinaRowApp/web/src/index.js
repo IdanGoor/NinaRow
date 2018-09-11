9f86a1a17f4ba6a313fa6f7b32c9a4c8115dee93
Idan Goor <idangoor1@gmail.com>
2018-09-11 16:28:12 +0300

    FIXED es6, babel, ReactJS node_nodules

---
+++ NinaRowApp/web/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+/* Directly adding react element */
+ReactDOM.render(
+    React.createElement('div',null, 'what what'),
+    document.getElementById("root")
+);

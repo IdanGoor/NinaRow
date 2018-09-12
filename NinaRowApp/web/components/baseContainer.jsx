08fcbf9076829c3659fec644a182d20947e9b5a3
Idan Goor <idangoor1@gmail.com>
2018-09-12 14:13:27 +0300

    UPDATED react, react-dom, webpack packages. ADDED utils and baseContainer

---
+++ NinaRowApp/web/components/baseContainer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../css/global.css";
+//import ViewManager from "./viewManager.jsx";
+
+export default class BaseContainer extends React.Component {
+  constructor() {
+    super();
+  };
+
+  render() {
+    return (
+        <div className={"base-background"}>
+          {/*<ViewManager/>*/}
+        </div>
+    )
+  }
+}
